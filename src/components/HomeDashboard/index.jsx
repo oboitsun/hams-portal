75b3cfec09d4c68cad4091e58b6aa51c7f92c765
Oleksandr Boitsun <thewip3out@gmail.com>
2021-11-18 12:52:41 +0200

    fix banner

--- src/components/HomeDashboard/index.jsx
+++ src/components/HomeDashboard/index.jsx
@@ -8,7 +8,7 @@ export default function HomeDashboard() {
         <Navigation />
       </div>
       <div className="banner relative">
-        <p className="text-7xl leading-none uppercase font-creep text-white absolute pb-10 left-0 w-full text-center top-1/4 transform -translate-y-1/2">
+        <p className="text-7xl leading-none uppercase font-creep text-white absolute pt-10 left-0 w-full text-center ">
           WElcome to the <br /> haunted house
         </p>
         <img className="" src="assets/welcome-to.png" alt="welcome to haunted hamsters" />
